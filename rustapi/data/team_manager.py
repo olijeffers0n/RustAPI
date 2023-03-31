a9a7d5207d7e4f3e8077360f84ca15bd19e5c8b6
Ollie <69084614+olijeffers0n@users.noreply.github.com>
2023-03-31 19:09:48 +0100

    Update Team Manager

--- rustapi/data/team_manager.py
+++ rustapi/data/team_manager.py
@@ -15,7 +15,33 @@ class TeamManager:
         with open("./rustapi/data/team.json", "r") as team:
             data = json.load(team)
             TeamManager._leader_id = data["leaderId"]
-            TeamManager._members = data["members"]
+            members = data["members"]
+
+        for member in members:
+            mem = AppTeamInfo.Member()
+            mem.steamId = member["id"]
+            mem.name = member["name"]
+            online = bool(random.randint(0, 1))
+            mem.isOnline = online
+            mem.spawnTime = int(time.time() - random.randint(0, 60*60*12))
+            mem.isAlive = bool(random.randint(0, 1))
+            mem.deathTime = int(time.time() - random.randint(0, 60*60*10)) if not mem.isAlive else 0
+            mem.x = random.randint(1000000, 5000000) / 1000
+            mem.y = random.randint(1000000, 5000000) / 1000
+
+            TeamManager._members.append(mem)
+
+
+    @staticmethod
+    def _offset_coords(member) -> AppTeamInfo.Member:
+
+        member.x += random.randint(-100, 100) / 10
+        member.y += random.randint(-100, 100) / 10
+
+        member.x = max(500, min(5500, member.x))
+        member.y = max(500, min(5500, member.y))
+
+        return member
 
     @staticmethod
     def get_team_info() -> AppTeamInfo:
@@ -24,18 +50,10 @@ class TeamManager:
 
         members = []
         for member in TeamManager._members:
-            mem = AppTeamInfo.Member()
-            mem.steamId = member["id"]
-            mem.name = member["name"]
-            mem.x = random.randint(0, 6000000) / 1000
-            mem.y = random.randint(0, 6000000) / 1000
-            online = bool(random.randint(0, 1))
-            mem.isOnline = online
-            mem.spawnTime = int(time.time() - random.randint(0, 60*60*12))
-            mem.isAlive = bool(random.randint(0, 1)) if online else False
-            mem.deathTime = int(time.time() - random.randint(0, 60*60*10)) if not mem.isAlive else 0
+            if member.isOnline:
+                member = TeamManager._offset_coords(member)
 
-            members.append(mem)
+            members.append(member)
 
         team_info.members.extend(members)
 
